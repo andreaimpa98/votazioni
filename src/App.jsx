a3e099e307a5ad6085904f9ed677abe179f29ffd
andreaimpa98 <58528128+andreaimpa98@users.noreply.github.com>
2026-02-20 15:27:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
 const CONFIG = {
   sessionKey: 'carnival-voted-v3', // ← cambia v1 → v2, v3... per resettare il "hai già votato" di tutti gli utenti
-  votingStartTime: new Date('2026-02-20T14:56:00').getTime(),
+  votingStartTime: new Date('2026-02-20T15:29:00').getTime(),
   votingDuration: 2 * 60 * 1000,
   adminPassword: 'carnevale2026',
   categories: [
