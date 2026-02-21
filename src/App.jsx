da082b7ed9893b1253e4793a6de50bd6fa7293c0
andreaimpa98 <58528128+andreaimpa98@users.noreply.github.com>
2026-02-21 01:43:08 +0100

    Update voting start time and duration

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const db = getFirestore(app);
 
 const CONFIG = {
   sessionKey: 'carnival-voted-v5', // ← cambia v1 → v2, v3... per resettare il "hai già votato" di tutti gli utenti
-  votingStartTime: new Date('2026-02-20T16:30:00').getTime(),
-  votingDuration: 10 * 60 * 1000,
+  votingStartTime: new Date('2026-02-2T23:30:00').getTime(),
+  votingDuration: 30 * 60 * 1000,
   adminPassword: 'carnevale2026',
   categories: [
     {
